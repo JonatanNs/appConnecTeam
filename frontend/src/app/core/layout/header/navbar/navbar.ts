474bd005103700e0694487f91484ab37d42f56a1
JonatanNs <nsualu.jonatan@gmail.com>
2026-08-11 15:55:05 +0200

    feat(frontend, layout): update header and navbar styles, enhance notification and profile dropdowns

--- frontend/src/app/core/layout/header/navbar/navbar.ts
+++ frontend/src/app/core/layout/header/navbar/navbar.ts
@@ -2,7 +2,13 @@ import {Component, signal} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {faBell} from '@fortawesome/free-regular-svg-icons';
-import {faChevronRight, faGear, faUser, } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCalendar,
+  faChevronRight,
+  faCircleQuestion,
+  faGear,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -15,6 +21,7 @@ export class Navbar {
   protected readonly faBell = faBell;
   protected readonly faGear = faGear;
   protected readonly faUser = faUser;
+  protected readonly faCalendar = faCalendar;
   protected readonly faChevronRight = faChevronRight;
   protected faRightFromBracket = faRightFromBracket;
 
@@ -26,5 +33,17 @@ export class Navbar {
 
   closeProfileMenu(): void {
     this.isProfileMenuOpen.set(false);
+  }
+
+  protected readonly faCircleQuestion = faCircleQuestion;
+
+  isNotificationMenuOpen = signal(false);
+
+  openNotificationMenu(): void {
+    this.isNotificationMenuOpen.set(true);
+  }
+
+  closeNotificationMenu(): void {
+    this.isNotificationMenuOpen.set(false);
   }
 }

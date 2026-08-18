48ff9e7a9665e1de7035c2cc22a0baf98e8208f5
JonatanNs <nsualu.jonatan@gmail.com>
2026-08-18 13:35:11 +0200

    feat(frontend, news): add news page with routing and layout for latest updates

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { MainLayout } from './core/layout/main-layout/main-layout';
 import {ADMIN_ROUTES} from './features/admin/admin.routes';
 import {LoginPage} from './features/auth/page/login-page/login-page';
 import { MESSAGING_ROUTES } from './features/messaging/messaging.route';
+import { NEWS_ROUTES } from './features/news/news.route';
 
 export const routes: Routes = [
   { path: 'auth/connexion', component: LoginPage },
   { path: 'annuaires', component: MainLayout, children: DIRECTORY_ROUTES },
   { path: 'admin', component: MainLayout, children: ADMIN_ROUTES },
   { path: 'messageries', component: MainLayout, children: MESSAGING_ROUTES },
+  { path: 'actualites', component: MainLayout, children: NEWS_ROUTES },
   { path: '', redirectTo: 'auth/connexion', pathMatch: 'full' },
   // { path: '**', component: NotFoundPage }, // à ajouter si tu n'as pas encore de catch-all
 ];
---
+++ frontend/src/app/features/news/news.route.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { NewsPage } from './pages/news-page/news-page';
+
+export const NEWS_ROUTES: Routes = [
+  { path: '', component: NewsPage, title: 'Actualités' },
+];
